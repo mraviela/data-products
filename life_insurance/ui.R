c406bcb3a7e48a40babe5e90090a2fa40535985b
Miguel Raviela <mraviela@gmail.com>
2018-07-15 21:39:32 -0500

    tab panels update

--- life_insurance/ui.R
+++ life_insurance/ui.R
@@ -10,30 +10,31 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(navbarPage("OnLine Life Insurance quotation",
+                   tabPanel("Quotations",br(),
+                            sidebarPanel(
+                              numericInput("age_input","Age:", value = 40, min = 18, max = 75, step = 1),
+                              checkboxInput("is_female","Check if you are female", value = FALSE),
+                              checkboxInput("is_smoker","Check if you are a smoker", value = TRUE),
+                              sliderInput("insured_val","Coverage selector", value = 250000, min = 50000, max = 1000000, step = 10000)
+                            ),
+                            mainPanel(
+                              h3("According to your data your annual insurance price is:"),
+                              textOutput("price_output"),
+                              h3("Main coverages prices."),
+                              plotOutput("distPlot")
+                            )
+                   ),
+                    tabPanel("About", 
+                                     h3("Description"),
+                                     h5("This application calculate the price for life insurance."),
+                                     h4("Elements"),
+                                     h5("Age - Your age"),
+                                     h5("Female - Check if you are a female"),
+                                     h5("Smoker - Check if you are a smoker"),
+                                     h5("Coverage selector - Select the desired coverage"))
+                            )
+          )
+
   
-  # Application title
-  titlePanel("OnLine Life Insurance quotation"),
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       numericInput("age_input","Age:", value = 40, min = 18, max = 75, step = 1),
-       checkboxInput("is_female","Check if you are female", value = FALSE),
-       checkboxInput("is_smoker","Check if you are a smoker", value = TRUE),
-       sliderInput("insured_val","Coverage selector", value = 250000, min = 50000, max = 1000000, step = 10000)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       
-       #h3("Calculation age"),
-       #textOutput("calc_age_output"),
-       h3("According to your data your annual insurance price is:"),
-       textOutput("price_output"),
-       h3("Main coverages prices."),
-       plotOutput("distPlot")
-       
-    )
-  )
-))
